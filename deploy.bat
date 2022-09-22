183cc136f53d1a59c4bee982af0c8683c82e4181
Hanbo Wang <hanbo.wang@smartekworks.com>
2022-09-22 10:02:14 +0800

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
-cd .\tools\ufile\
-.\filemgr-win64.exe --action sync --bucket swathub-download --dir ..\..\packages --prefix packages/
+c:\ufile\filemgr-win64.exe --action sync --bucket swathub-download --dir .\packages --prefix packages/
+aws s3 sync ./packages s3://download.swathub.com/packages
